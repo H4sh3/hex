50294e86a94a1dd655899ab1cccbaa2cc581a0c7
h4sh3 <h4sh3@git.com>
2020-07-16 14:10:50 +0200

    init

---
+++ backend/app.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const app = express();
+const yup = require('yup');
+
+const { nanoid } = require('nanoid');
+
+require('dotenv').config({ path: '../.env' });
+
+const u = process.env.MONGO_INITDB_ROOT_USERNAME;
+const p = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const dbn = process.env.MONGO_INITDB_DATABASE;
+const dbp = process.env.MONGO_PORT;
+
+const mongoUri = `${u}:${p}@db:${dbp}/${dbn}`
+console.log(mongoUri)
+const db = require('monk')(mongoUri)
+db.then(() => {
+    console.log('Connected to db')
+})
+const urls = db.get('urls');
+
+urls.createIndex({ slug: 1 }, { unique: true })
+
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json()); // accept only json
+app.use(express.static('./public'));
+
+const schema = yup.object().shape({
+    slug: yup.string().trim().matches(/^[\w]+$/),
+    url: yup.string().trim().url().required()
+});
+
+app.get('/:id', async (req, res) => {
+    const { id: slug } = req.params;
+    try {
+        const url = await urls.find({ slug });
+        if (url) {
+            res.redirect(url[0].url)
+        }
+    } catch (error) {
+        res.redirect('/')
+    }
+});
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body;
+    if (!slug) {
+        slug = nanoid(5)
+    }
+
+    try {
+        await schema.validate({
+            slug,
+            url,
+        })
+
+        const existing = await urls.findOne({ slug });
+        if (existing) {
+            throw new Error('Slug in use.')
+        }
+
+        slug = slug.toLowerCase();
+        const newUrl = { url, slug }
+        const created = await urls.insert(newUrl)
+        res.json(created);
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'uups' : error.message
+    })
+})
+/*
+app.get('/:id', (req, res) => {
+    // TODO: redir url
+});
+
+app.get('/url/:id', (req, res) => {
+    // TODO: get a short url by id
+});
+*/
+const port = process.env.NODE_PORT || 7331
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app)
+.listen(port, () => {
+    console.log(`Listening on https://localhost:${port}`);
+}) 
